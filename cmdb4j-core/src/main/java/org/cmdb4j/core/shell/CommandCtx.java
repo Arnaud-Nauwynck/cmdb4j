3ea175ed6c5a915e5ac436f5197912d891be4b4d
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2016-04-07 22:28:02 +0200

    work in progress

--- cmdb4j-core/src/main/java/org/cmdb4j/core/shell/CommandCtx.java
+++ cmdb4j-core/src/main/java/org/cmdb4j/core/shell/CommandCtx.java
@@ -1,5 +1,24 @@
 package org.cmdb4j.core.shell;
 
-public interface CommandCtx {
+public class CommandCtx {
 
+    private final String name;
+
+    // ------------------------------------------------------------------------
+    
+    public CommandCtx(String name) {
+        this.name = name;
+    }
+
+    // ------------------------------------------------------------------------
+    
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "CommandCtx[" + name + "]";
+    }
+    
 }
---
+++ cmdb4j-core/src/main/java/org/cmdb4j/core/shell/CommandSession.java
@@ -0,0 +1,45 @@
+package org.cmdb4j.core.shell;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ */
+public class CommandSession {
+
+    private Map<String,CommandCtx> contexts = new HashMap<>();
+    
+    private CommandCtx currentContext;
+    
+    // ------------------------------------------------------------------------
+
+    public CommandSession() {
+    }
+
+    // ------------------------------------------------------------------------
+
+    public void putNewContext(String name) {
+        CommandCtx ctx = new CommandCtx(name);
+        contexts.put(name, ctx);
+    }
+    
+    public void removeContext(String name) {
+        contexts.remove(name);
+        if (currentContext != null && currentContext.getName().equals(name)) {
+            currentContext = null;
+        }
+    }
+    
+    public CommandCtx getContext(String name) {
+        return contexts.get(name);
+    }
+    
+    public void setCurrentContext(String name) {
+        currentContext = contexts.get(name);
+    }
+    
+    public CommandCtx getCurrentContext() {
+        return currentContext;
+    }
+}
